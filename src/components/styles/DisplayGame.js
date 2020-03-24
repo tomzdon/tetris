d373524183565acfcc4457befaade374a5c44359
tomzdon <tomekk.wlodarczyk@gmail.com>
2020-03-24 14:35:27 +0100

    created model style

---
+++ src/components/styles/DisplayGame.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const DisplayGame = styled.div`
+display: flex;
+align-items: flex-start;
+padding: 40px;
+margin: 0 auto;
+max-width: 900px;
+
+aside {
+  width: 100%;
+  max-width: 200px;
+  display: block;
+  padding: 0 20px;
+}
+`;
