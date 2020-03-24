f9d87632381547a180e8142a0d372b15eb7e6e3a
tomzdon <tomekk.wlodarczyk@gmail.com>
2020-03-24 14:37:19 +0100

    added constants

---
+++ src/constants/figure.js
@@ -0,0 +1,59 @@
+export const figure = {
+  0: { positionStart: [[0]], color: "0, 0, 0" },
+  I: {
+    color: "80, 227, 230",
+    positionStart: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0]
+    ]
+  },
+  J: {
+    positionStart: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0]
+    ],
+    color: "36, 95, 223"
+  },
+  L: {
+    positionStart: [
+      [0, "L", 0],
+      [0, "L", 0],
+      [0, "L", "L"]
+    ],
+    color: "223, 173, 36"
+  },
+  O: {
+    positionStart: [
+      ["O", "O"],
+      ["O", "O"]
+    ],
+    color: "223, 217, 36"
+  },
+  S: {
+    positionStart: [
+      [0, "S", "S"],
+      ["S", "S", 0],
+      [0, 0, 0]
+    ],
+    color: "48, 211, 56"
+  },
+  T: {
+    positionStart: [
+      [0, 0, 0],
+      ["T", "T", "T"],
+      [0, "T", 0]
+    ],
+    color: "132, 61, 198"
+  },
+  Z: {
+    positionStart: [
+      ["Z", "Z", 0],
+      [0, "Z", "Z"],
+      [0, 0, 0]
+    ],
+    color: "227, 78, 78"
+  }
+};
